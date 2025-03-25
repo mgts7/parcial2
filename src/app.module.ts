132b9f82d6ce23243397fac82199a6c1b69447ad
mgts7 <miguel.amc4261@gmail.com>
2025-03-25 01:38:49 -0500

    Ahora si2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { ContestantsModule } from './contestants/contestants.module';
 import { BattlesModule } from './battles/battles.module';
 import { DictatorsModule } from './dictators/dictators.module';
 import { SponsorsModule } from './sponsors/sponsors.module';
 import { TransactionsModule } from './transactions/transactions.module';
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
+
+
+
 
 @Module({
   imports: [ContestantsModule, BattlesModule, DictatorsModule, SponsorsModule, TransactionsModule,
