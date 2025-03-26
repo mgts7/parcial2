3392cd2fb97017f27dc50ac88eb092de91f8f936
mgts7 <miguel.amc4261@gmail.com>
2025-03-26 10:16:41 -0500

    Pagina prin

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { Response } from 'express';
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHome() {
-    return { message: 'Servidor funcionando correctamente' };
+  getHome(@Res() res: Response) {
+    const filePath = join(__dirname, '..', 'public', 'index.html'); // Ruta del archivo
+    return res.sendFile(filePath);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: './.env' }); // Carga el archivo .env
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
